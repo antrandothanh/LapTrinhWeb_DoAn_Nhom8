953203b5c5d8f9e43daaea2bc0f27aa6562de587
huynhtri123 <huynhmtri1211@gmail.com>
2023-11-13 21:41:53 +0700

    #8 Model

---
+++ src/main/java/business/Product.java
@@ -0,0 +1,8 @@
+package business;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+public class Product {
+    //xoá class này nha, này hùng làm. Tui tạo cho nó không báo lỗi thôi
+}
