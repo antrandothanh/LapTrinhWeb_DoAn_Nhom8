2d43988681d9eb3445fd263df209222d9c3c1ae5
HunggSamm <hunggsamm@gmail.com>
2023-11-13 21:44:38 +0700

    #9 business

---
+++ src/main/java/business/Brand.java
@@ -0,0 +1,31 @@
+package business;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Brand implements Serializable{
+    private String code;
+    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public String getCode()
+    {
+        return code;
+    }
+    public void setCode(String code)
+    {
+        this.code=code;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+}
+
